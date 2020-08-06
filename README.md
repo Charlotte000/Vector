c8b1ea51bb7fafa566577582ac54ec0cd79355cb
Charlotte000 <ramil.mifteev@yandex.ru>
2020-08-06 19:26:12 +0300

    Added Vector2 and Vector3 classes. Changed readme. Added lengthSq func

--- README.md
+++ README.md
@@ -1,86 +1,96 @@
-# Vector
+# Vector2 / Vector3
 
-It is a simple Vector class in Python. Vector class provides max 3 dimensional vectors and its converting.  
-It also supports the use of mathematical expressions, such as +, -, /, *
+It is a simple Vector class in Python. Vector class provides 2/3 dimensional vectors and its converting.  
+It also supports the use of mathematical expressions, such as +, -, /, *, //
 
 ---
-_Vector(x, y, z)_ - you can pass x, y, z or list.
+_Vector2(x, y)_ - you can pass x, y or list.  
+_Vector3(x, y, z)_ - you can pass x, y, z or list.
 ```python
-v1 = Vector(1, 2, 3)
-v2 = Vector(5, 6)
+v1 = Vector3(1, 2, 3)
+v2 = Vector2(5, 6)
 ```
 ---
-_Vector.setLength(length)_ - sets the vector length to _length_
+_Vector2.setLength(length)_ - sets the vector length to _length_
+_Vector3.setLength(length)_ - same
 ```python
 v1.setLength(1)
 ```
 ---
-_Vector.length()_ - returns the vector length
+_Vector2.length()_ - return vectors length  
+_Vector3.length()_ - same
 ```python
 v1.length()
 ```
 ---
-_Vector.round()_ - returns the rounded projection of the vector as a tuple
+_Vector2.lengthSq()_ - return vectors length squared  
+_Vector3.lengthSq()_ - same
 ```python
-v1.round()
+v1.lengthSq()
 ```
 ---
-_Vector.fromPoints(point1, point2)_ - returns the vector between point1 and point2
+_Vector2.fromPoints(point1, point2)_ - returns the vector between point1 and point2  
+_Vector3.fromPoints(point1, point2)_ - same, but _point_ containts 3 numbers
 ```python
-Vector.fromPoints([1, 2, 3], [4, 5])
+Vector3.fromPoints([1, 2, 3], [4, 5, -5])
 ```
 ---
-_Vector.fromAngle(angle)_ - returns the unit vector with _angle_
+_Vector2.fromAngle(angle)_ - returns the unit vector with _angle_  
+_Vector3.fromAngle(angle)_ - same, but _z_ coordinate is 0
 ```python
-Vector.fromAngle(pi / 2)
+Vector2.fromAngle(pi / 2)
 ```
 ---
-_Vector.average(*vectors)_ - returns the mean vector
+_Vector2.average(*vectors)_ - returns the mean vector  
+_Vector3.average(*vectors)_ - same
 ```python
-Vector.average(v1, v2)
+Vector2.average(v1, v2)
 ```
 ---
-_Vector.random2D()_ - returns a 2d random unit vector
+_Vector2.random()_ - returns a 2d random unit vector  
+_Vector3.random()_ - same, but in 3 dimensions
 ```python
-Vector.random2D()
+Vector2.random()
 ```
 ---
-_Vector.random3D()_ - returns a 3d random unit vector
-```python
-Vector.random3D()
-```
----
-_Vector.angle()_ - returns the vector angle between -pi and pi on XY plane
+_Vector2.angle()_ - return vectors angle between -pi and pi on XY plane  
+_Vector3.angle()_ - same
 ```python
 v1.angle()
 ```
 ---
-_Vector.setAngle(angle)_ - sets the vector _angle_
+_Vector2.setAngle(angle)_ - sets the vector _angle_  
+_Vector3.setAngle(angle)_ - same, but z coordinate doesn't change
 ```python
 v1.setAngle(3.14)
 ```
 ---
-_Vector.angleBetween(v1, v2)_ - returns the angle between v1 and v2 on XY plane between -pi and pi
+_Vector2.angleBetween(v1, v2)_ - returns the angle between v1 and v2 on XY plane between -pi and pi  
+_Vector3.angleBetween(v1, v2)_ - same
 ```python
-Vector.angleBetween(v1, v2)
+Vector2.angleBetween(v1, v2)
 ```
 ---
-_Vector.rotate2d(angle)_ - rotates the vector by Z axis by _angle_ in radians
+_Vector2.rotate(angle)_ - rotates the vector by Z axis by _angle_  
+_Vector3.rotate(angle)_ - same
 ```python
-v1.rotate2D(pi)
+v1.rotate(pi)
 ```
 ---
-_Vector.copy()_ - returns the copy of the vector
+_Vector2.copy()_ - returns the copy of the vector  
+_Vector3.copy()_ - same
 ```python
 v1.copy()
 ```
 ---
-_Vector.constrain(minValue, maxValue)_ - constraints vector's length between _minValue_ and _maxValue_
+_Vector2.constrain(minValue, maxValue)_ - constraints vector's length between _minValue_ and _maxValue_  
+_Vector3.constrain(minValue, maxValue)_ - same
 ```python
 v1.constrain(1, 2)
 ```
 ---
-_Vector.DEGREES_ - bool variable that enables working with degrees. False by default
+_Vector2.DEGREES_ - bool variable that enables working with degrees. False by default  
+_Vector3.DEGREES_ - same
 ```python
-Vector.DEGREES = True
+Vector2.DEGREES = True
 ```
--- vector.py
+++ vector.py
@@ -1,8 +1,192 @@
 from math import sqrt, cos, sin, atan2, degrees, radians
 from random import uniform
 
+class Vector2:
+    '''2D Vector'''
+    DEGREES = False
 
-class Vector:
+    def __init__(self, x=0, y=0):
+        if type(x) in [list, tuple]:
+            if len(x) == 1:
+                self.x = x[:]
+                self.y = 0
+            elif len(x) == 2:
+                self.x, self.y = x[:]
+        else:
+            self.x = x
+            self.y = y
+
+    def setLength(self, length):
+        '''Sets the vector length'''
+        ve = self.copy()
+        if ve.length() > 0:
+            ve *= length / ve.length()
+            self.x, self.y = ve.x, ve.y
+        return self
+
+    def length(self):
+        '''Returns the vectors length'''
+        return sqrt(pow(self.x, 2) + pow(self.y, 2))
+
+    def lengthSq(self):
+        '''Returns the vectors length squared'''
+        return pow(self.x, 2) + pow(self.y, 2)
+
+    def angle(self):
+        '''Return vectors angle between -pi and pi'''
+        a = atan2(self.y, self.x)
+        if Vector2.DEGREES:
+            return degrees(a)
+        return a
+
+    def setAngle(self, angle):
+        '''Sets the vector angle'''
+        v = Vector2.fromAngle(angle)
+        v *= self.length()
+        self.x = v.x
+        self.y = v.y
+        return self
+
+    def rotate(self, angle):
+        '''Rotates the vector by Z axis by angle'''
+        if Vector2.DEGREES:
+            angle = radians(angle)
+        x = self.x * cos(angle) - self.y * sin(angle)
+        y = self.x * sin(angle) + self.y * cos(angle)
+
+        self.x, self.y = x, y
+        return self
+
+    def copy(self):
+        return Vector2(self.x, self.y)
+
+    def constrain(self, minValue, maxValue):
+        le = self.length()
+        if le > maxValue:
+            self.setLength(maxValue)
+        elif le < minValue:
+            self.setLength(minValue)
+        return self
+
+    @staticmethod
+    def fromPoints(point1, point2):
+        '''Returns the vector between point1 and point2'''
+        return Vector2(*point2) - Vector2(*point1)
+
+    @staticmethod
+    def fromAngle(angle):
+        '''Returns the unit vector with angle'''
+        v = Vector2(1, 0)
+        v.rotate(angle)
+        return v
+
+    @staticmethod
+    def average(*vectors):
+        '''Returns the mean vector'''
+        v = Vector2()
+        for vec in vectors:
+            v += vec
+        v /= len(vectors)
+        return v
+
+    @staticmethod
+    def random():
+        '''Returns a random unit vector'''
+        a = Vector2(uniform(-1, 1), uniform(-1, 1))
+        a.setLength(1)
+        return a
+
+    @staticmethod
+    def angleBetween(v1, v2):
+        a = atan2(v1.x * v2.y - v1.y * v2.x, v1.x * v2.x + v1.y * v2.y)
+        if Vector2.DEGREES:
+            return degrees(a)
+        return a
+
+    def __iadd__(self, vector):
+        self.x += vector.x
+        self.y += vector.y
+        return self
+
+    def __isub__(self, vector):
+        self.x -= vector.x
+        self.y -= vector.y
+        return self
+
+    def __imul__(self, number):
+        self.x *= number
+        self.y *= number
+        return self
+
+    def __itruediv__(self, number):
+        self.x /= number
+        self.y /= number
+        return self
+
+    def __ifloordiv__(self, number):
+        self.x //= number
+        self.y //= number
+        return self 
+
+    def __add__(self, vector):
+        v = self.copy()
+        v += vector
+        return v
+
+    def __sub__(self, vector):
+        v = self.copy()
+        v -= vector
+        return v
+
+    def __mul__(self, number):
+        v = self.copy()
+        v *= number
+        return v
+
+    def __truediv__(self, number):
+        v = self.copy()
+        v /= number
+        return v
+
+    def __floordiv__(self, number):
+        v = self.copy()
+        v //= number
+        return v
+
+    def __str__(self):
+        return f'Vector2[{self.x}, {self.y}]'
+
+    def __getitem__(self, item):
+        return [self.x, self.y][item]
+
+    def __setitem__(self, item, value):
+        if item == 0:
+            self.x = value
+        elif item == 1:
+            self.y = value
+        else:
+            raise IndexError
+
+    def __round__(self):
+        return Vector2(round(self.x), round(self.y))
+
+    def __iter__(self):
+        return iter([self.x, self.y])
+
+    def __eq__(self, other):
+        if self.x == other.x and self.y == other.y:
+            return True
+        return False
+
+    def __lt__(self, vector):
+        return self.length() < vector.length()
+
+    def __le__(self, vector):
+        return self.length() <= vector.length()
+
+
+class Vector3:
+    '''3D Vector'''
     DEGREES = False
 
     def __init__(self, x=0, y=0, z=0):
@@ -21,6 +205,7 @@ class Vector:
             self.z = z
 
     def setLength(self, length):
+        '''Sets the vector length'''
         ve = self.copy()
         if ve.length() > 0:
             ve *= length / ve.length()
@@ -28,23 +213,31 @@ class Vector:
         return self
 
     def length(self):
+        '''Returns the vectors length'''
         return sqrt(pow(self.x, 2) + pow(self.y, 2) + pow(self.z, 2))
 
+    def lengthSq(self):
+        '''Returns the vectors length squared'''
+        return pow(self.x, 2) + pow(self.y, 2) + pow(self.z, 2)
+
     def angle(self):
+        '''Return vectors angle between -pi and pi on XY plane'''
         a = atan2(self.y, self.x)
-        if Vector.DEGREES:
+        if Vector3.DEGREES:
             return degrees(a)
         return a
 
     def setAngle(self, angle):
-        v = Vector.fromAngle(angle)
+        '''Sets the vector angle on XY plane'''
+        v = Vector3.fromAngle(angle)
         v *= self.length()
         self.x = v.x
         self.y = v.y
         return self
 
     def rotate(self, angle):
-        if Vector.DEGREES:
+        '''Rotates the vector by Z axis by angle'''
+        if Vector3.DEGREES:
             angle = radians(angle)
         x = self.x * cos(angle) - self.y * sin(angle)
         y = self.x * sin(angle) + self.y * cos(angle)
@@ -53,7 +246,7 @@ class Vector:
         return self
 
     def copy(self):
-        return Vector(self.x, self.y, self.z)
+        return Vector3(self.x, self.y, self.z)
 
     def constrain(self, minValue, maxValue):
         le = self.length()
@@ -62,74 +255,99 @@ class Vector:
         elif le < minValue:
             self.setLength(minValue)
         return self
-    
+
     @staticmethod
     def fromPoints(point1, point2):
-        return Vector(*point2) - Vector(*point1)
+        '''Returns the vector between point1 and point2'''
+        return Vector3(*point2) - Vector3(*point1)
 
     @staticmethod
     def fromAngle(angle):
-        v = Vector(1, 0)
+        '''Returns the unit vector with angle. z coordinate is 0'''
+        v = Vector3(1, 0, 0)
         v.rotate(angle)
         return v
-    
+
     @staticmethod
     def average(*vectors):
-        v = Vector()
+        '''Returns the mean vector'''
+        v = Vector3()
         for vec in vectors:
             v += vec
         v /= len(vectors)
         return v
 
     @staticmethod
-    def random2D():
-        a = Vector(uniform(-1, 1), uniform(-1, 1))
+    def random():
+        '''Returns a random unit vector'''
+        a = Vector3(uniform(-1, 1), uniform(-1, 1), uniform(-1, 1))
         a.setLength(1)
         return a
 
-    @staticmethod
-    def random3D():
-        a = Vector(uniform(-1, 1), uniform(-1, 1), uniform(-1, 1))
-        a.setLength(1)
-        return a
-    
     @staticmethod
     def angleBetween(v1, v2):
         a = atan2(v1.x * v2.y - v1.y * v2.x, v1.x * v2.x + v1.y * v2.y)
-        if Vector.DEGREES:
+        if Vector3.DEGREES:
             return degrees(a)
         return a
 
+    def __iadd__(self, vector):
+        self.x += vector.x
+        self.y += vector.y
+        self.z += vector.z
+        return self
+
+    def __isub__(self, vector):
+        self.x -= vector.x
+        self.y -= vector.y
+        self.z -= vector.z
+        return self
+
+    def __imul__(self, number):
+        self.x *= number
+        self.y *= number
+        self.z *= number
+        return self
+
+    def __itruediv__(self, number):
+        self.x /= number
+        self.y /= number
+        self.z /= number
+        return self
+
+    def __ifloordiv__(self, number):
+        self.x //= number
+        self.y //= number
+        self.z //= number
+        return self
+
     def __add__(self, vector):
         v = self.copy()
-        v.x += vector.x
-        v.y += vector.y
-        v.z += vector.z
+        v += vector
         return v
 
     def __sub__(self, vector):
         v = self.copy()
-        v.x -= vector.x
-        v.y -= vector.y
-        v.z -= vector.z
+        v -= vector
         return v
 
     def __mul__(self, number):
         v = self.copy()
-        v.x *= number
-        v.y *= number
-        v.z *= number
+        v *= number
         return v
 
     def __truediv__(self, number):
         v = self.copy()
-        v.x /= number
-        v.y /= number
-        v.z /= number
+        v /= number
+        return v
+
+    def __floordiv__(self, number):
+        v = self.copy()
+        v //= number
         return v
 
     def __str__(self):
-        return f'Vector[{self.x}, {self.y}, {self.z}]'
+        return f'Vector3[{self.x}, {self.y}, {self.z}]'
 
     def __getitem__(self, item):
         return [self.x, self.y, self.z][item]
@@ -145,7 +363,7 @@ class Vector:
             raise IndexError
 
     def __round__(self):
-        return Vector(round(self.x), round(self.y), round(self.z))
+        return Vector3(round(self.x), round(self.y), round(self.z))
 
     def __iter__(self):
         return iter([self.x, self.y, self.z])
@@ -159,4 +377,4 @@ class Vector:
         return self.length() < vector.length()
 
     def __le__(self, vector):
-        return (self < vector) or (self == vector)
+        return self.length() <= vector.length()
